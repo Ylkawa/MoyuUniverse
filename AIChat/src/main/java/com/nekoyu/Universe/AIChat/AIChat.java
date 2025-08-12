d7c9da7f796c49f801f3e26368c740fd870cc694
Ylkawa <102233701+Ylkawa@users.noreply.github.com>
2025-08-12 19:16:02 +0000

    （未测试）aichat可以通过配置文件为assistant添加tools

--- AIChat/src/main/java/com/nekoyu/Universe/AIChat/AIChat.java
+++ AIChat/src/main/java/com/nekoyu/Universe/AIChat/AIChat.java
@@ -117,6 +117,11 @@ public class AIChat extends Law {
                     Object provider = Universe.Providers.get(cfg.getNode("Provider").toString());
                     if (provider instanceof DeepSeekChannel dsc) {
                         Assistant assistant = dsc.getAssistant(cfg.getNode("Model").toString());
+                        if (cfg.getNode("Tools") instanceof List) {
+                            for (String tool : (List) cfg.getNode("Tools")) {
+                                if (deepSeekTools.get(tool) != null) assistant.addTool(deepSeekTools.get(tool));
+                            } // 为assistant添加指定的tools // 如果不存在这个tool就不添加
+                        }
                         try {
                             var response = assistant.request(ml);
                             mcm.action.reply(response.choices[0].message.content);
