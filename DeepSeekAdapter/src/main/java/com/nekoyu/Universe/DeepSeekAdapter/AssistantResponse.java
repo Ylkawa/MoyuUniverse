60c9c3955806c7a6d59a2fd82b37695dc7e77e18
Ylkawa <imylk@nekoyu.com>
2025-10-15 18:52:05 +0800

    删掉了几个无关紧要的Usage值

--- DeepSeekAdapter/src/main/java/com/nekoyu/Universe/DeepSeekAdapter/AssistantResponse.java
+++ DeepSeekAdapter/src/main/java/com/nekoyu/Universe/DeepSeekAdapter/AssistantResponse.java
@@ -29,8 +29,5 @@ public class AssistantResponse {
         public int prompt_tokens;
         public int completion_tokens;
         public int total_tokens;
-        public Map<String, Integer> prompt_tokens_details;
-        public int prompt_cache_hit_tokens;
-        public int prompt_cache_miss_tokens;
     }
 }
