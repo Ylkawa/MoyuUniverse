f66501dd877237beb06fb72c3c482cdfe6f8fa49
Ylkawa <imylk@nekoyu.com>
2025-11-01 19:09:38 +0800

    。。一个很蠢的小错误

--- Universe/src/main/java/com/nekoyu/Universe/API/MessageChannel/MessageField/MsgField.java
+++ Universe/src/main/java/com/nekoyu/Universe/API/MessageChannel/MessageField/MsgField.java
@@ -6,11 +6,11 @@ public abstract class MsgField {
     public String type;
     public boolean isSolved = false;
 
+    public void solve(AtomicBoolean flag) {}
     /**
      * 以文本形式呈现本段的内容
      * 为大语言模型应用设计的功能
      * @return 本段内容描述
      */
-    public void solve(AtomicBoolean flag) {}
     public abstract String getAsString();
 }
