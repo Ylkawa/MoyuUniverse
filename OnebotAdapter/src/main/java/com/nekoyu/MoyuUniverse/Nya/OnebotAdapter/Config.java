1d59f0ed001242c335dfe5dbc63ad8adddf8d63f
Ylkawa <imylk@nekoyu.com>
2025-11-28 16:03:43 +0800

    OnebotAdapter 将弃用 ConfigureProcessor

---
+++ OnebotAdapter/src/main/java/com/nekoyu/MoyuUniverse/Nya/OnebotAdapter/Config.java
@@ -0,0 +1,7 @@
+package com.nekoyu.MoyuUniverse.Nya.OnebotAdapter;
+
+public class Config {
+    String ID;
+    String Token;
+    String URI;
+}
--- OnebotAdapter/src/main/java/com/nekoyu/MoyuUniverse/Nya/OnebotAdapter/OnebotAdapter.java
+++ OnebotAdapter/src/main/java/com/nekoyu/MoyuUniverse/Nya/OnebotAdapter/OnebotAdapter.java
@@ -1,8 +1,10 @@
 package com.nekoyu.MoyuUniverse.Nya.OnebotAdapter;
 
+import com.google.gson.Gson;
 import com.nekoyu.Universe.ConfigureProcessor.CFGFileSyntaxException;
 import com.nekoyu.Universe.ConfigureProcessor.ConfigureProcessor;
 import com.nekoyu.Universe.LawsLoader.Law;
+import org.apache.logging.log4j.core.util.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -18,6 +20,7 @@ import java.util.*;
  * OnebotAdapter 的作用应该是，解析配置文件，然后按各 Channel 的配置 new channel，再运行 channel ，注册到 宇宙消息通道
  */
 public class OnebotAdapter extends Law {
+    Gson gson = new Gson();
     boolean isReady = true;
     Logger logger = LoggerFactory.getLogger(this.getClass());
     List<OnebotChannel> ocs = new ArrayList<>();
@@ -31,18 +34,18 @@ public class OnebotAdapter extends Law {
             logger.info(String.valueOf(configDir.mkdirs()));
         }
         // 按照目录下的 配置文件 依次加载若干个 OnebotChannel
-        File configFileLoc = new File("./config/Onebot/Onebot.yml.template");
+        File configFileLoc = new File("./config/Onebot/Onebot.json.template");
         if (!configFileLoc.exists()) { // 这个配置文件不存在，创建一个
-            try (InputStream in = getClass().getClassLoader().getResourceAsStream("config-template/Onebot.yml")) {
+            try (InputStream in = getClass().getClassLoader().getResourceAsStream("config-template/Onebot.json")) {
                 if (in == null) {
-                    logger.error("无法在JAR包中找到默认配置文件 config-template/Onebot.yml");
+                    logger.error("无法在JAR包中找到默认配置文件 config-template/Onebot.json");
                     stop();
                     isReady = false;
                     return false;
                 }
                 // 复制文件
                 Files.copy(in, configFileLoc.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                logger.info("模板配置文件已生成: ./config/Onebot/Onebot.yml.template");
+                logger.info("模板配置文件已生成: ./config/Onebot/Onebot.json.template");
             } catch (IOException e) {
                 logger.error("生成模板配置文件时出错", e);
                 stop();
@@ -61,6 +64,20 @@ public class OnebotAdapter extends Law {
             if (cfgFiles != null) {
                 for (File file : cfgFiles) {
                     // 筛选出 后缀名为 yml 的文件
+                    if (file.getName().endsWith(".json")) {
+                        try {
+                            Config config = gson.fromJson(new FileReader(file), Config.class);
+                            OnebotChannel oc = new OnebotChannel(config.ID);
+                            oc.token = config.Token;
+                            oc.uri = new URI(config.URI);
+                            ocs.add(oc);
+                            logger.info("已载入 Onebot 配置 {}", oc.ID);
+                        } catch (FileNotFoundException e) {
+                            throw new RuntimeException(e);
+                        } catch (URISyntaxException e) {
+                            logger.warn("URI无效, {} 将不会被加载", file.getName());
+                        }
+                    }
                     if (file.getName().endsWith(".yml")) {
                         ConfigureProcessor cp = null;
                         try {
@@ -81,7 +98,7 @@ public class OnebotAdapter extends Law {
                             break;
                         }
                         ocs.add(oc);
-                        logger.info("已载入 Onebot 配置 {}", oc.ID);
+                        logger.info("已载入 Onebot 配置 {} 警告: 请将配置文件修改成json模式", oc.ID);
                     }
                 }
             }
