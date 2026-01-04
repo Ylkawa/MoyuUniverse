25873b0e08f04f9c5c89ad3e0ebd79af42fc3f8e
Ylkawa <imylk@nekoyu.com>
2026-01-04 20:25:48 +0800

    修复无法正确识别@全体的问题

--- Universe/src/main/java/com/nekoyu/Universe/API/MessageChannel/MessageField/AtField.java
+++ Universe/src/main/java/com/nekoyu/Universe/API/MessageChannel/MessageField/AtField.java
@@ -3,12 +3,11 @@ package com.nekoyu.Universe.API.MessageChannel.MessageField;
 import com.nekoyu.Universe.API.MessageChannel.Account;
 
 public class AtField extends MsgField {
-    Account target;
+    public Account target;
 
     public AtField(Account target) {
         super.type = "at";
         this.target = target;
-        super.isSolved = true;
     }
 
     public String getAsString() {
